4f6b457c0de5f917708b90282562442de32a01c6
Siddharth Garg <60679173+siddharthgarg7@users.noreply.github.com>
2024-06-23 22:50:35 -0400

    Maximum sub array

---
+++ MaximumSubarray.java
@@ -0,0 +1,14 @@
+public class MaximumSubarray {
+
+  public int maxSubArray(int[] nums) {
+    int res = Integer.MIN_VALUE, sum = 0;
+    for (int i = 0; i < nums.length; i++) {
+      sum += nums[i];
+      res = Math.max(res, sum);
+      if (sum < 0)
+        sum = 0;
+    }
+    return res;
+  }
+
+}
